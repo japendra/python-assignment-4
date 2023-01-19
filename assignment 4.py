39ac0011b65205874015e37b2d1fd990d91c212d
japendra <121219369+japendra@users.noreply.github.com>
2023-01-19 15:44:26 +0530

    Add files via upload

---
+++ assignment 4.py
@@ -0,0 +1,75 @@
+#ans1
+a=int(input("ENTER YOUR MARKS:-"))
+if a<25:
+    print("YOUR GRADE:- F")
+if 25<=a<45:
+    print("YOUR GRADE:- E")
+if 45<=a<50:
+    print("YOUR GRADE:-D")
+if 50<=a<60:
+    print("YOUR GRADE:- C")
+if 60<=a<80:
+    print("YOUR GRADE:- B")
+if 80<=a<=100:
+    print("YOUR GRADE:- A")
+elif a>100:
+    print("PLEASE ENTER A VALID NUMBER")
+
+#ans2
+b=int(input("ENTER ANY YEAR:-"))
+if b % 4==0:
+   print("IT IS A LEAP YEAR")
+elif b % 4!=0:
+    print("IT IS NOT A LEAP YEAR")
+
+#ans3
+c=int(input("Question 1:- 2 x 3 ="))
+if c==6:
+    print("Correct!")
+elif c!=6:
+    print("Wrong!")
+d=int(input("Question 2:- 8 x 7 ="))
+if d==56:
+    print("Correct!")
+elif c!=56:
+    print("Wrong!")
+e=int(input("Question 3:- 9 x 3 ="))
+if e==27:
+    print("Correct!")
+elif e!=27:
+    print("Wrong!")
+f=int(input("Question 4:- 9 x 5="))
+if f==45:
+    print("Correct!")
+elif f!=45:
+    print("Wrong!")
+g=int(input("Question 5:- 4 x 3 ="))
+if g==12:
+    print("Correct!")
+elif g!=12:
+    print("Wrong!")
+h=int(input("Question 6:- 6 x 8 ="))
+if h==48:
+    print("Correct!")
+elif h!=48:
+    print("Wrong!")
+i=int(input("Question 7:- 8 x 9 ="))
+if i==72:
+    print("Correct!")
+elif i!=72:
+    print("Wrong!")
+j=int(input("Question 8:- 9 x 2 ="))
+if j==18:
+    print("Correct!")
+elif j!=18:
+    print("Wrong!")
+k=int(input("Question 9:- 5 x 3 ="))
+if k==15:
+    print("Correct!")
+elif k!=15:
+    print("Wrong!")
+l=int(input("Question 10:- 2 x 3 ="))
+if l==6:
+    print("Correct!")
+elif l!=27:
+    print("Wrong!")
